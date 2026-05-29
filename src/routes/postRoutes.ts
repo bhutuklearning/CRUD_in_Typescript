21534621a49e7f012995cd8db9176d496adfbafe
bhutuklearning <amritanshugoutam.dev@gmail.com>
2026-05-29 13:59:31 +0530

    Added validation schema for the Post Model

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -7,12 +7,14 @@ import {
     deletePost,
 } from '../controllers/postController.js';
 import { protect } from '../middlewares/auth.js';
+import { validate } from '../middlewares/validate.js';
+import {createPostSchema} from '../validators/postSchemas.js';
 
 const router = Router();
 
 router.get('/', protect, getPosts);
 router.get('/:id', protect, getPost);
-router.post('/', protect, createPost);
+router.post('/', protect, validate(createPostSchema), createPost);
 router.put('/:id', protect, updatePost);
 router.delete('/:id', protect, deletePost);
 
